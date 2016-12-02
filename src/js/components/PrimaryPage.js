87fba1ea7810bfd0230c87e5c27964493a4d7cae
karatechops <contact@lazers.tv>
2016-12-02 15:20:47 -0500

    Add Header and Footer components.

--- src/js/components/PrimaryPage.js
+++ src/js/components/PrimaryPage.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
 import Card from 'grommet/components/Card';
+import Footer from 'grommet/components/Footer';
+import Header from 'grommet/components/Header';
 import Heading from 'grommet/components/Heading';
 import Hero from 'grommet/components/Hero';
+import Menu from 'grommet/components/Menu';
 import Paragraph from 'grommet/components/Paragraph';
+import GrommetIcon from 'grommet/components/icons/base/BrandGrommetOutline';
+import SearchIcon from 'grommet/components/icons/base/Search';
 import NewsFeed from './NewsFeed';
 
 export default class PrimaryPage extends Component {
   render() {
     return (
       <Box colorIndex="light-1">
+        <Header justify="center" colorIndex="neutral-4">
+          <Box size={{width: {max: 'xxlarge'}}} direction="row"
+            responsive={false} justify="start" align="center" 
+            pad={{horizontal: 'medium'}}>
+            <GrommetIcon colorIndex="brand" />
+            <Box pad="small" />
+            <Menu label="Label" inline={true} direction="row">
+              <Anchor href="#">Solutions</Anchor>
+              <Anchor href="#">Services</Anchor>
+              <Anchor href="#">Products</Anchor>
+              <Anchor href="#">About Us</Anchor>
+              <Anchor href="#">Support</Anchor>
+            </Menu>
+            <Box flex="grow" align="end">
+              <SearchIcon />
+            </Box>
+          </Box>
+        </Header>
         <Box>
           <Hero size="large" backgroundImage="/img/marquee.jpg" colorIndex="light-1">
             <Card
@@ -51,37 +74,38 @@ export default class PrimaryPage extends Component {
           </Box>
           <NewsFeed />
         </Box>
-
-        <Box colorIndex="neutral-1" pad="large" align="center">
-          <Box className="footer-cards-container" pad={{vertical: "medium"}}
-            size={{width: "xxlarge"}} direction="row">
-            <Card
-              pad={{horizontal: "large"}}
-              contentPad="medium"
-              basis="1/2"
-              direction="row"
-              heading="Lorem ipsum dolor sit amet"
-              label="Label"
-              link={
-                <Anchor href="http://www.grommet.io/docs/" primary={true}>
-                  Learn More
-                </Anchor>
-              }
-              separator="right" />
-            <Card
-              pad={{horizontal: "large"}}
-              contentPad="medium"
-              basis="1/2"
-              direction="row"
-              heading="Lorem ipsum dolor sit amet"
-              label="Label"
-              link={
-                <Anchor href="http://www.grommet.io/docs/" primary={true}>
-                  Learn More
-                </Anchor>
-              } />
+        <Footer full="horizontal">
+          <Box colorIndex="neutral-1" pad="large" align="center" full="horizontal">
+            <Box className="footer-cards-container" pad={{vertical: "medium"}}
+              size={{width: "xxlarge"}} direction="row">
+              <Card
+                pad={{horizontal: "large"}}
+                contentPad="medium"
+                basis="1/2"
+                direction="row"
+                heading="Lorem ipsum dolor sit amet"
+                label="Label"
+                link={
+                  <Anchor href="http://www.grommet.io/docs/" primary={true}>
+                    Learn More
+                  </Anchor>
+                }
+                separator="right" />
+              <Card
+                pad={{horizontal: "large"}}
+                contentPad="medium"
+                basis="1/2"
+                direction="row"
+                heading="Lorem ipsum dolor sit amet"
+                label="Label"
+                link={
+                  <Anchor href="http://www.grommet.io/docs/" primary={true}>
+                    Learn More
+                  </Anchor>
+                } />
+            </Box>
           </Box>
-        </Box>
+        </Footer>
       </Box>
     );
   }
