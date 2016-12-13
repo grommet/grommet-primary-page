71ebf87028e3c92b7512492be1d27f8fc9a0dde0
karatechops <contact@lazers.tv>
2016-12-13 12:56:11 -0500

    Fix Header responsive bug.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -9,18 +9,19 @@ import SearchIcon from 'grommet/components/icons/base/Search';
 export default function AppHeader (props) {
   return (
     <Header justify="center" colorIndex="neutral-4">
-      <Box size={{width: {min: 'xxlarge'}}} direction="row"
-        responsive={false} justify="start" align="center" 
-        pad={{horizontal: 'medium'}}>
+      <Box size={{width: {max: 'xxlarge'}}} direction="row"
+        responsive={false} justify="start" align="center"
+        pad={{horizontal: 'medium'}} flex="grow">
         <GrommetIcon colorIndex="brand" size="large" />
         <Box pad="small" />
-        <Menu label="Label" inline={true} direction="row">
+        <Menu label="Label" inline={true} direction="row" flex="grow">
           <Anchor href="#">Solutions</Anchor>
           <Anchor href="#">Services</Anchor>
           <Anchor href="#">Products</Anchor>
           <Anchor href="#">About Us</Anchor>
           <Anchor href="#">Support</Anchor>
         </Menu>
+
         <Box flex="grow" align="end">
           <SearchIcon />
         </Box>
